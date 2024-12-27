95af7783ca512c793a557a684060d2cf6cf64fc4
Mattia Iaria <mattia.iaria@brt.it>
2024-12-27 18:46:56 +0100

    feat: Registerpage

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -9,13 +9,77 @@ import { StyleClassModule } from 'primeng/styleclass';
 import { InputGroupModule } from 'primeng/inputgroup';
 import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
 import { KeyFilterModule } from 'primeng/keyfilter';
+import { RegisterForm, passwordRegex, phoneNumberRegex } from '../../types/register';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { PasswordModule } from 'primeng/password';
+import { DividerModule } from 'primeng/divider';
 
 @Component({
   selector: 'app-register',
-  imports: [InputGroupModule, InputGroupAddonModule, ButtonModule, Checkbox, StyleClassModule, InputTextModule, TranslatePipe, Card, RouterModule, KeyFilterModule],
+  imports: [
+    DividerModule,
+    PasswordModule,
+    FormsModule,
+    ReactiveFormsModule,
+    InputGroupModule,
+    InputGroupAddonModule,
+    ButtonModule,
+    Checkbox,
+    StyleClassModule,
+    InputTextModule,
+    TranslatePipe,
+    Card,
+    RouterModule,
+    KeyFilterModule,
+  ],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
 
+
+  form: FormGroup = new FormGroup({
+    firstName: new FormControl<string>('', [Validators.required, Validators.minLength(2)]),
+    lastName: new FormControl<string>('',[Validators.required, Validators.minLength(2)]),
+    email: new FormControl<string>('', [Validators.required, Validators.email]),
+    phoneNumber: new FormControl<string>('', [Validators.required, Validators.pattern(phoneNumberRegex)]),
+    password: new FormControl<string>('', [Validators.required, Validators.pattern(passwordRegex)]),
+    confirmPassword: new FormControl<string>('', [Validators.required, Validators.pattern(passwordRegex)]),
+    gdpr: new FormControl<boolean>(false, Validators.requiredTrue)
+  },passwordMatchValidator);
+
+  getPasswordErrors(type: 'lowercase' | 'uppercase' | 'minimum' | 'special' | 'numeric'){
+    const value = this.form.get('password')!.value as string
+    switch(type){
+      case "lowercase": return /(?=.*[a-z])/.exec(value)
+      case "uppercase": return /(?=.*[A-Z])/.exec(value)
+      case "minimum": return /(?=.{8,})/.exec(value)
+      case "numeric": return /(?=.*\d)/.exec(value)
+      case "special": return /(?=.*[^a-zA-Z0-9])/.exec(value)
+    }
+  }
+  isPasswordFieldPristine(){
+    return this.form.get('password')?.pristine ?? false
+  }
+  
+  submit(){
+    if(this.form.invalid)
+      return;
+
+    const registerForm: RegisterForm = {
+      email: this.form.get('email')!.value,
+      firstName: this.form.get('firstName')!.value,
+      lastName: this.form.get('lastName')!.value,
+      password: this.form.get('password')!.value,
+      phoneNumber: this.form.get('phoneNumber')!.value
+    }
+    console.log(registerForm);
+    
+  }
+
+}
+
+function passwordMatchValidator(g: any): any {
+  return g.get('password')?.value === g.get('confirmPassword')?.value
+     ? null : {'mismatch': true};
 }
---
+++ src/types/register.ts
@@ -0,0 +1,10 @@
+export type RegisterForm = {
+    firstName: string,
+    lastName: string,
+    email: string,
+    phoneNumber: string,
+    password: string
+}
+export const phoneNumberRegex = /^[\+]?[0-9]{0,3}\W?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im
+export const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9])(?=.{8,})/;
+export const strongPasswordRegex = /^(?=(.*[a-z]){2,})(?=(.*[A-Z]){2,})(?=(.*[0-9]){2,})(?=(.*[^a-zA-Z0-9]){2,})(?=.{12,})(?!.*(1234|password|qwerty|abc)).*/
